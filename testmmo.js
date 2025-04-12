542f2b6951c49149605e3a30c043bbc6db3a313a
Im random <fabiosilvatabom1975@gmail.com>
2025-04-12 11:48:02 -0300

    Update testmmo.js

--- testmmo.js
+++ testmmo.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ server });
 const players = new Map();
 
 let nextPlayerId = 1;
@@ -48,4 +48,4 @@ function broadcast(msg) {
   }
 }
 
-console.log('Works);
+console.log('Works');
