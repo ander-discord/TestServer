b890fe116f67137d836ca841c53a091ee4502562
Im random <fabiosilvatabom1975@gmail.com>
2025-07-10 19:45:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const wss = new WebSocket.Server({ port: 8080 }, () => {
 
 let clients = [];
 let blocks = [];
-const sudoIds = ['xeno!ander'];
-const sudoPasswords = { 'xeno!ander': 'Pizza!!!' };
-let authenticatedSudo = {};
-let alreadyauth = [];
-
 let pendingUpdateBlocks;
 
 function broadcast(data) {
@@ -18,7 +13,7 @@ function broadcast(data) {
       c.ws.send(JSON.stringify(data));
     }
   }
-} 
+}
 
 wss.on('connection', (ws) => {
   const id = Math.random().toString(36);
@@ -29,7 +24,12 @@ wss.on('connection', (ws) => {
     .filter(c => c.id !== id)
     .map(c => c.player);
 
-  ws.send(JSON.stringify({type: 'init', me: id, blocks: blocks, players: otherPlayers}));
+  ws.send(JSON.stringify({
+    type: 'init',
+    me: id,
+    blocks: blocks,
+    players: otherPlayers
+  }));
 
   ws.on('message', (msg) => {
     let data;
@@ -49,33 +49,8 @@ wss.on('connection', (ws) => {
       }
     } else if (data.type === 'block-update') {
       pendingUpdateBlocks = data.blocks;
-      //broadcast({ type: 'blocks', blocks: data.blocks })
     } else if (data.type === 'chat') {
-      if (!msg.startsWith('/auth')) broadcast({ type: 'chat', message: data.message });
-
-      const msg = data.message.content;
-      const sender = data.message.username;
-
-      if (msg === `/auth ${sudoPasswords[sender]}` && sudoIds.includes(sender)) {
-        authenticatedSudo[socket.id] = true;
-        broadcast({ type: 'chat', message: { username: 'SERVER', content: `${sender} authenticated as sudo.` } });
-      } else if (authenticatedSudo[socket.id]) {
-          if (msg === '/reset' && sudoIds.includes(sender)) {
-          blocks = [];
-          pendingUpdateBlocks = [];
-          broadcast({ type: 'blocks', blocks: [] });
-          broadcast({ type: 'chat', message: { username: 'SERVER', content: `World reset by ${sender}.` } });
-
-        } else if (msg.startsWith('/sudo ') && sudoIds.includes(sender)) {
-          const code = msg.slice(6);
-          try {
-            const result = eval(code);
-            broadcast({ type: 'chat', message: { username: 'SERVER', content: `OUTPUT: ${result}` } });
-          } catch (err) {
-            broadcast({ type: 'chat', message: { username: 'SERVER', content: `ERROR: ${err.message}` } });
-          }
-        }
-      }
+      broadcast({ type: 'chat', message: data.message });
     }
   });
 
@@ -83,7 +58,7 @@ wss.on('connection', (ws) => {
     clients = clients.filter(c => c.id !== id);
     broadcast({ type: 'leave', id });
   });
-})
+});
 
 setInterval(() => {
   if (pendingUpdateBlocks) {
@@ -91,4 +66,4 @@ setInterval(() => {
     broadcast({ type: 'blocks', blocks: pendingUpdateBlocks });
     pendingUpdateBlocks = null;
   }
-}, 100);
+}, 10);
